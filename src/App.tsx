c65867176111820cdf27924af7651a05eca2e3aa
Ikramullah <ikramullahzx@gmail.com>
2022-03-15 22:44:19 +0800

    feat(scss,tailwind): add packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import './scss/index.scss'
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello worlds!
+      </h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
