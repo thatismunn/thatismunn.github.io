3b8b7dcd89e5695376b5465afbb7a2bbcd2e04c1
Ikramullah <ikramullahzx@gmail.com>
2022-05-03 16:45:46 +0800

    feat(porto): fancy pagination

--- src/Porto/Porto.tsx
+++ src/Porto/Porto.tsx
@@ -1,7 +1,30 @@
+import { useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, Mousewheel } from 'swiper'
+
 function Porto() {
+  const [page, setPage] = useState(0)
+
   return (
     <div className="porto">
-      Nyerah saya pak wkwkkw
+      <Swiper
+        className='h-screen w-screen'
+        spaceBetween={15}
+        direction={'vertical'}
+        pagination={{clickable: true}}
+        mousewheel={true}
+        modules={[Pagination, Mousewheel]}
+        onSlideChange={swiper => setPage(swiper.realIndex)}
+      >
+        {
+          // Simulasi ganti page
+          "Slide|".repeat(3).split('|').slice(0,3).map((v, i) => (
+            <SwiperSlide key={i} className='bg-gray-300 flex'>
+              <p className="m-auto text-9xl">{v} {page + 1}</p>
+            </SwiperSlide>
+          ))
+        }
+      </Swiper>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import Resume from './Resume/Resume'
 import Porto from './Porto/Porto'
 import './index.css'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
 
 ReactDOM.render(
   <React.StrictMode>
