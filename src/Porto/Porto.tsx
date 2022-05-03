c334969063f1a0ee34e7d38686e7d35fad57f318
Ikramullah <ikramullahzx@gmail.com>
2022-05-03 15:44:34 +0800

    deps(add): swiper + typical

--- src/Porto/Porto.tsx
+++ src/Porto/Porto.tsx
@@ -1,9 +1,7 @@
-import TypingAnimation from '../Function/TypingAnimation'
-
 function Porto() {
   return (
     <div className="porto">
-      <TypingAnimation text='Hello world!' speed={100} blinkDuration={100} blinkIdling={2000} />
+      Nyerah saya pak wkwkkw
     </div>
   )
 }
