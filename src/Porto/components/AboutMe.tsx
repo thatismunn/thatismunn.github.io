eb86e278a15753c957a655fbc7b70ec3f130d43c
Ikramullah <ikramullahzx@gmail.com>
2022-05-05 17:33:13 +0800

    feat(porto/aboutme): mobile

--- src/Porto/components/AboutMe.tsx
+++ src/Porto/components/AboutMe.tsx
@@ -47,8 +47,31 @@ function AboutMe({active}: {active: boolean}) {
       </div>
 
       {/* Mobile */}
-      <div className='grid-cols-1 lg:hidden grid'>
-        {/*  */}
+      <div className='lg:hidden h-screen bg-cover bg-center animate__animated animate__fadeIn animate__faster' style={{backgroundImage: `url(${FullPict})`}}>
+        <div className="bg-base-100/80 backdrop-blur-sm h-full flex">
+          <div className="my-auto mx-24 text-primary">
+            <p className='text-5xl font-bold pb-3 underline decoration-1 underline-offset-[0.5rem] text-center animate__animated animate__fadeInDown animate__faster'>
+              About <span className="text-secondary">Me</span>
+            </p>
+            <p className="py-3 text-xl text-center animate__animated animate__fadeInRight animate__faster">
+              An IT undergraduate student who is trying to establish himself to be able to compete in today's technological era. Have a desire to become a teacher in the future and develop technology both nationally and internationally.
+            </p>
+            <div className="pt-2 text-lg animate__animated animate__fadeInUp animate__faster">
+              {
+                data.map((v, i) => (
+                  <div className="grid grid-cols-4" key={i}>
+                    <div className="col-span-1">
+                      <span className="text-secondary">{v[0]}</span>
+                    </div>
+                    <div className="col-span-3">
+                      <span className="text-primary">{v[1]}</span>
+                    </div>
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
